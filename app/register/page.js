cc515498ea887cd043f4adce21189351f2affe9f
survittal <149235755+survittal@users.noreply.github.com>
2025-04-12 12:00:37 +0530

    register page update1

--- app/register/page.js
+++ app/register/page.js
@@ -274,7 +274,7 @@ export default function Home() {
             Submit
           </button>
           <button
-            onClick={router.push("/")}
+            onClick=""
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Back to Home
