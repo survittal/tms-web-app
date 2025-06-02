67727582406d8c2fdadd0b6aeed0ea113613f038
survittal <149235755+survittal@users.noreply.github.com>
2025-06-02 21:40:41 +0530

    printing-error-1

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+/*const nextConfig = {};*/
+const nextConfig = {
+  webpack: (config) => {
+    // Handle PDF renderer
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+
+    return config;
+  },
+  transpilePackages: ["@react-pdf/renderer"],
+};
 
 export default nextConfig;
